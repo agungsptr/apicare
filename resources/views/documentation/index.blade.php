712586175b03fc8ecdcc481b5d8c6d8fe2d4375b
agungsptr <agung.eka1999@gmail.com>
2020-08-01 22:02:36 +0000

    add documentatioin page

---
+++ resources/views/documentation/index.blade.php
@@ -0,0 +1,255 @@
+@extends('layouts.app')
+
+@section('content')
+<!-- ======= Hero Section ======= -->
+<section id="hero" class="d-flex align-items-center">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 pt-5 pt-lg-0 order-2 order-lg-1 d-flex flex-column justify-content-center">
+                <h1 data-aos="fade-up">Apicare - API Car Rental</h1>
+                <h2 data-aos="fade-up" data-aos-delay="400">This API for Car Rental System</h2>
+                <div data-aos="fade-up" data-aos-delay="800">
+                    <a href="#about" class="btn-get-started scrollto">Get Started</a>
+                </div>
+            </div>
+            <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="fade-left" data-aos-delay="200">
+                <img src="{{ asset('vendor/Vesperr/assets/img/hero-img.png') }}" class="img-fluid animated" alt="">
+            </div>
+        </div>
+    </div>
+
+</section><!-- End Hero -->
+
+<main id="main">
+
+    <!-- ======= Clients Section ======= -->
+    <section id="clients" class="clients clients">
+        <div class="container">
+
+            <div class="row">
+                <div class="col-lg-4 col-md-4 col-6"></div>
+                <div class="col-lg-4 col-md-4 col-6">
+                    <img src="{{ asset('vendor/Vesperr/assets/img/clients/ubg.png') }}" class="img-fluid" alt="" data-aos="zoom-in"
+                        data-aos-delay="200">
+                </div>
+                <div class="col-lg-4 col-md-4 col-6"></div>
+            </div>
+
+        </div>
+    </section><!-- End Clients Section -->
+
+    <!-- ======= About Us Section ======= -->
+    <section id="about" class="about">
+        <div class="container">
+
+            <div class="section-title" data-aos="fade-up">
+                <h2>About Us</h2>
+            </div>
+
+            <div class="row content">
+                <div class="col-lg-6" data-aos="fade-up" data-aos-delay="150">
+                    <p>
+                        Ini adalah Project UAS Web Services yaitu membuat RESTful API dengan tema yang
+                        sudah ditentukan. Dalam hal ini saya mendapatkan tema Penyewaan, maka dari itu
+                        saya membuat Apicare (API Car Rental) atau API penyewaan mobil.
+                    </p>
+                    <ul>
+                        <li><i class="ri-check-double-line"></i> Nama : Agung Eka Saputra</li>
+                        <li><i class="ri-check-double-line"></i> NIM : 1710520118</li>
+                        <li><i class="ri-check-double-line"></i> Semester : 6</li>
+                    </ul>
+                </div>
+                <div class="col-lg-6 pt-4 pt-lg-0" data-aos="fade-up" data-aos-delay="300">
+                    <p>
+                        Apicare ini menyediakan data User (Admin), data Customer (Penyewa), data Car
+                        (Mobil), dan data Rental (Penyewaan).
+                    </p>
+                    <a href="#services" class="btn-learn-more">Learn More</a>
+                </div>
+            </div>
+
+        </div>
+    </section><!-- End About Us Section -->
+
+    <!-- ======= Services Section ======= -->
+    <section id="services" class="services">
+        <div class="container">
+
+            <div class="section-title" data-aos="fade-up">
+                <h2>Documentation</h2>
+                <p>Daftar CRUDS yang dapat dilakukan pada Apicare</p>
+            </div>
+
+            <div class="row">
+                <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+                    <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
+                        <div class="icon"><i class="bx bx-user"></i></div>
+                        <h4 class="title"><a href="">User</a></h4>
+                        <p class="description">Menyediakan data User atau admin dari rental mobil</p>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+                    <div class="icon-box" data-aos="fade-up" data-aos-delay="200">
+                        <div class="icon"><i class="bx bx-group"></i></div>
+                        <h4 class="title"><a href="">Customer</a></h4>
+                        <p class="description">Menyediakan data Customer atau penyewa mobil</p>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+                    <div class="icon-box" data-aos="fade-up" data-aos-delay="300">
+                        <div class="icon"><i class="bx bx-car"></i></div>
+                        <h4 class="title"><a href="">Car</a></h4>
+                        <p class="description">Menyediakan data Car atau mobil</p>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+                    <div class="icon-box" data-aos="fade-up" data-aos-delay="400">
+                        <div class="icon"><i class="bx bx-book-alt"></i></div>
+                        <h4 class="title"><a href="">Rental</a></h4>
+                        <p class="description">Menyediakan data Rental atau penyewaan</p>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section><!-- End Services Section -->
+
+    <!-- ======= Team Section ======= -->
+    <section id="team" class="team section-bg">
+        <div class="container">
+
+            <div class="section-title" data-aos="fade-up">
+                <h2>Team</h2>
+                <p>Tim pembuat Apicare</p>
+            </div>
+
+            <div class="row">
+
+                <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+                    <div class="member" data-aos="fade-up" data-aos-delay="100">
+                        <div class="member-img">
+                            <img src="{{ asset('vendor/Vesperr/assets/img/team/1.png') }}" class="img-fluid" alt="">
+                            <div class="social">
+                                <a href="https://twitter.com/_agungsptr" class="twitter"><i
+                                        class="icofont-twitter"></i></a>
+                            </div>
+                        </div>
+                        <div class="member-info">
+                            <h4>Agung Saputra</h4>
+                            <span>Chief Executive Officer</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+                    <div class="member" data-aos="fade-up" data-aos-delay="200">
+                        <div class="member-img">
+                            <img src="{{ asset('vendor/Vesperr/assets/img/team/2.jpg') }}" class="img-fluid" alt="">
+                            <div class="social">
+                                <a href="https://web.facebook.com/people/Agung-Saputra/100010969991694"
+                                    class="facebook"><i class="icofont-facebook"></i></a>
+                            </div>
+                        </div>
+                        <div class="member-info">
+                            <h4>Agung Saputra</h4>
+                            <span>Product Manager</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+                    <div class="member" data-aos="fade-up" data-aos-delay="300">
+                        <div class="member-img">
+                            <img src="{{ asset('vendor/Vesperr/assets/img/team/3.jpg') }}" class="img-fluid" alt="">
+                            <div class="social">
+                                <a href="https://www.instagram.com/__agungsptr/" class="instagram"><i
+                                        class="icofont-instagram"></i></a>
+                            </div>
+                        </div>
+                        <div class="member-info">
+                            <h4>Agung Saputra</h4>
+                            <span>CTO</span>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-3 col-md-6 d-flex align-items-stretch">
+                    <div class="member" data-aos="fade-up" data-aos-delay="400">
+                        <div class="member-img">
+                            <img src="{{ asset('vendor/Vesperr/assets/img/team/4.jpg') }}" class="img-fluid" alt="">
+                            <div class="social">
+                                <a href="https://www.linkedin.com/in/agung-eke-saputra/" class="linkedin"><i
+                                        class="icofont-linkedin"></i></a>
+                            </div>
+                        </div>
+                        <div class="member-info">
+                            <h4>Agung Saputra</h4>
+                            <span>Accountant</span>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </section><!-- End Team Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+        <div class="container">
+
+            <div class="section-title" data-aos="fade-up">
+                <h2>Contact Us</h2>
+            </div>
+
+            <div class="row">
+
+                <div class="col-lg-6 col-md-6" data-aos="fade-up" data-aos-delay="100">
+                    <div class="contact-about">
+                        <h3>Apicare</h3>
+                        <p>Ini adalah Project UAS Web Services yaitu membuat RESTful API dengan tema yang
+                            sudah ditentukan. Dalam hal ini saya mendapatkan tema Penyewaan, maka dari itu
+                            saya membuat Apicare (API Car Rental) atau API penyewaan mobil.</p>
+                        <div class="social-links">
+                            <a href="https://twitter.com/_agungsptr" class="twitter"><i class="icofont-twitter"></i></a>
+                            <a href="https://web.facebook.com/people/Agung-Saputra/100010969991694" class="facebook"><i
+                                    class="icofont-facebook"></i></a>
+                            <a href="https://www.instagram.com/__agungsptr/" class="instagram"><i
+                                    class="icofont-instagram"></i></a>
+                            <a href="https://www.linkedin.com/in/agung-eke-saputra/" class="linkedin"><i
+                                    class="icofont-linkedin"></i></a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-6 col-md-6 mt-4 mt-md-0" data-aos="fade-up" data-aos-delay="200">
+                    <div class="info">
+                        <div>
+                            <i class="ri-map-pin-line"></i>
+                            <p>Jl. Pendidikan B III<br>Mataram, Nusa Tenggara Barat</p>
+                        </div>
+
+                        <div>
+                            <i class="ri-mail-send-line"></i>
+                            <p>agung.eka1999@gmail.com</p>
+                        </div>
+
+                        <div>
+                            <i class="ri-phone-line"></i>
+                            <p>+62 875 7574 4674</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section><!-- End Contact Section -->
+
+</main>
+<!-- End #main -->
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Car Rental API</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="{{ asset('vendor/Vesperr/assets/img/favicon.png') }}" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+    rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="{{ asset('vendor/Vesperr/assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('vendor/Vesperr/assets/vendor/icofont/icofont.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('vendor/Vesperr/assets/vendor/remixicon/remixicon.css') }}" rel="stylesheet">
+  <link href="{{ asset('vendor/Vesperr/assets/vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('vendor/Vesperr/assets/vendor/owl.carousel/assets/owl.carousel.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('vendor/Vesperr/assets/vendor/venobox/venobox.css" rel="stylesheet') }}">
+  <link href="{{ asset('vendor/Vesperr/assets/vendor/aos/aos.css') }}" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="{{ asset('vendor/Vesperr/assets/css/style.css') }}" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Vesperr - v2.2.0
+  * Template URL: https://bootstrapmade.com/vesperr-free-bootstrap-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center">
+
+      <div class="logo mr-auto">
+        <div class="row">
+          <a href="{{ url('/') }}"><img src="{{ asset('vendor/Vesperr/assets/img/logo.png') }}" alt="" class="img-fluid mr-2"></a>
+          <h1 class="text-light"><a href="{{ url('/') }}"><span>Apicare</span></a></h1>
+        </div>
+      </div>
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li class="active"><a href="#">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li class="drop-down"><a href="#services">Documentation</a>
+            <ul>
+              <li><a href="#">User</a></li>
+              <li><a href="#">Customer</a></li>
+              <li><a href="#">Car</a></li>
+              <li><a href="#">Rental</a></li>
+            </ul>
+          </li>
+          <li><a href="#team">Team</a></li>
+          <li><a href="#contact">Contact</a></li>
+
+          <li class="get-started"><a href="#about">Get Started</a></li>
+        </ul>
+      </nav><!-- .nav-menu -->
+
+    </div>
+  </header><!-- End Header -->
+
+  @yield('content')
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="container">
+      <div class="row d-flex align-items-center">
+        <div class="col-lg-6 text-lg-left text-center">
+          <div class="copyright">
+            &copy; Copyright <strong>agungsptr</strong>. All Rights Reserved
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <nav class="footer-links text-lg-right text-center pt-2 pt-lg-0">
+            <a href="#" class="scrollto">Home</a>
+            <a href="#about" class="scrollto">About</a>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="{{ asset('vendor/Vesperr/assets/vendor/jquery/jquery.min.js') }}"></script>
+  <script src="{{ asset('vendor/Vesperr/assets/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('vendor/Vesperr/assets/vendor/jquery.easing/jquery.easing.min.js') }}"></script>
+  <script src="{{ asset('vendor/Vesperr/assets/vendor/php-email-form/validate.js') }}"></script>
+  <script src="{{ asset('vendor/Vesperr/assets/vendor/waypoints/jquery.waypoints.min.js') }}"></script>
+  <script src="{{ asset('vendor/Vesperr/assets/vendor/counterup/counterup.min.js') }}"></script>
+  <script src="{{ asset('vendor/Vesperr/assets/vendor/owl.carousel/owl.carousel.min.js') }}"></script>
+  <script src="{{ asset('vendor/Vesperr/assets/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+  <script src="{{ asset('vendor/Vesperr/assets/vendor/venobox/venobox.min.js') }}"></script>
+  <script src="{{ asset('vendor/Vesperr/assets/vendor/aos/aos.js') }}"></script>
+
+  <!-- Template Main JS File -->
+  <script src="{{ asset('vendor/Vesperr/assets/js/main.js') }}"></script>
+
+</body>
+
+</html>
